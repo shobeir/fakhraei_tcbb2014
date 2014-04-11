a032d207feafa99e7c3aeaff2949c280e6891347
Shobeir Fakhraei <shobeir@umiacs.umd.edu>
2014-04-11 12:13:52 -0400

    updates

--- src/main/matlab/Perlmans_method/code/generate_dataset.m
+++ src/main/matlab/Perlmans_method/code/generate_dataset.m
@@ -13,25 +13,25 @@ for i=1:nDrugs,
         Perlman_Dataset((i-1)*nTargets+j,1)=i;
         Perlman_Dataset((i-1)*nTargets+j,2)=j;
         
-        Perlman_Dataset((i-1)*nTargets+j,3)=calcualte_feature(i,j,DrugSim_ATCHierDrugsCommonSimilarityMat,TargetSim_GOTargetsCommonSimilarityMat,Interactions_Matrix);
-        Perlman_Dataset((i-1)*nTargets+j,4)=calcualte_feature(i,j,DrugSim_ATCHierDrugsCommonSimilarityMat,TargetSim_distTargetsCommonSimilarityMat,Interactions_Matrix);
-        Perlman_Dataset((i-1)*nTargets+j,5)=calcualte_feature(i,j,DrugSim_ATCHierDrugsCommonSimilarityMat,TargetSim_seqTargetsCommonSimilarityMat,Interactions_Matrix);
+        Perlman_Dataset((i-1)*nTargets+j,3)=calculate_feature(i,j,DrugSim_ATCHierDrugsCommonSimilarityMat,TargetSim_GOTargetsCommonSimilarityMat,Interactions_Matrix);
+        Perlman_Dataset((i-1)*nTargets+j,4)=calculate_feature(i,j,DrugSim_ATCHierDrugsCommonSimilarityMat,TargetSim_distTargetsCommonSimilarityMat,Interactions_Matrix);
+        Perlman_Dataset((i-1)*nTargets+j,5)=calculate_feature(i,j,DrugSim_ATCHierDrugsCommonSimilarityMat,TargetSim_seqTargetsCommonSimilarityMat,Interactions_Matrix);
         
-        Perlman_Dataset((i-1)*nTargets+j,6)=calcualte_feature(i,j,DrugSim_chemicalDrugsCommonSimilarityMat,TargetSim_GOTargetsCommonSimilarityMat,Interactions_Matrix);
-        Perlman_Dataset((i-1)*nTargets+j,7)=calcualte_feature(i,j,DrugSim_chemicalDrugsCommonSimilarityMat,TargetSim_distTargetsCommonSimilarityMat,Interactions_Matrix);
-        Perlman_Dataset((i-1)*nTargets+j,8)=calcualte_feature(i,j,DrugSim_chemicalDrugsCommonSimilarityMat,TargetSim_seqTargetsCommonSimilarityMat,Interactions_Matrix);
+        Perlman_Dataset((i-1)*nTargets+j,6)=calculate_feature(i,j,DrugSim_chemicalDrugsCommonSimilarityMat,TargetSim_GOTargetsCommonSimilarityMat,Interactions_Matrix);
+        Perlman_Dataset((i-1)*nTargets+j,7)=calculate_feature(i,j,DrugSim_chemicalDrugsCommonSimilarityMat,TargetSim_distTargetsCommonSimilarityMat,Interactions_Matrix);
+        Perlman_Dataset((i-1)*nTargets+j,8)=calculate_feature(i,j,DrugSim_chemicalDrugsCommonSimilarityMat,TargetSim_seqTargetsCommonSimilarityMat,Interactions_Matrix);
         
-        Perlman_Dataset((i-1)*nTargets+j,9)=calcualte_feature(i,j,DrugSim_ligandJaccardDrugsCommonSimilarityMat,TargetSim_GOTargetsCommonSimilarityMat,Interactions_Matrix);
-        Perlman_Dataset((i-1)*nTargets+j,10)=calcualte_feature(i,j,DrugSim_ligandJaccardDrugsCommonSimilarityMat,TargetSim_distTargetsCommonSimilarityMat,Interactions_Matrix);
-        Perlman_Dataset((i-1)*nTargets+j,11)=calcualte_feature(i,j,DrugSim_ligandJaccardDrugsCommonSimilarityMat,TargetSim_seqTargetsCommonSimilarityMat,Interactions_Matrix);
+        Perlman_Dataset((i-1)*nTargets+j,9)=calculate_feature(i,j,DrugSim_ligandJaccardDrugsCommonSimilarityMat,TargetSim_GOTargetsCommonSimilarityMat,Interactions_Matrix);
+        Perlman_Dataset((i-1)*nTargets+j,10)=calculate_feature(i,j,DrugSim_ligandJaccardDrugsCommonSimilarityMat,TargetSim_distTargetsCommonSimilarityMat,Interactions_Matrix);
+        Perlman_Dataset((i-1)*nTargets+j,11)=calculate_feature(i,j,DrugSim_ligandJaccardDrugsCommonSimilarityMat,TargetSim_seqTargetsCommonSimilarityMat,Interactions_Matrix);
         
-        Perlman_Dataset((i-1)*nTargets+j,12)=calcualte_feature(i,j,DrugSim_newCMapJaccardDrugsCommonSimilarityMat,TargetSim_GOTargetsCommonSimilarityMat,Interactions_Matrix);
-        Perlman_Dataset((i-1)*nTargets+j,13)=calcualte_feature(i,j,DrugSim_newCMapJaccardDrugsCommonSimilarityMat,TargetSim_distTargetsCommonSimilarityMat,Interactions_Matrix);
-        Perlman_Dataset((i-1)*nTargets+j,14)=calcualte_feature(i,j,DrugSim_newCMapJaccardDrugsCommonSimilarityMat,TargetSim_seqTargetsCommonSimilarityMat,Interactions_Matrix);
+        Perlman_Dataset((i-1)*nTargets+j,12)=calculate_feature(i,j,DrugSim_newCMapJaccardDrugsCommonSimilarityMat,TargetSim_GOTargetsCommonSimilarityMat,Interactions_Matrix);
+        Perlman_Dataset((i-1)*nTargets+j,13)=calculate_feature(i,j,DrugSim_newCMapJaccardDrugsCommonSimilarityMat,TargetSim_distTargetsCommonSimilarityMat,Interactions_Matrix);
+        Perlman_Dataset((i-1)*nTargets+j,14)=calculate_feature(i,j,DrugSim_newCMapJaccardDrugsCommonSimilarityMat,TargetSim_seqTargetsCommonSimilarityMat,Interactions_Matrix);
         
-        Perlman_Dataset((i-1)*nTargets+j,15)=calcualte_feature(i,j,DrugSim_pSideEffectDrugsCommonSimilarityMat,TargetSim_GOTargetsCommonSimilarityMat,Interactions_Matrix);
-        Perlman_Dataset((i-1)*nTargets+j,16)=calcualte_feature(i,j,DrugSim_pSideEffectDrugsCommonSimilarityMat,TargetSim_distTargetsCommonSimilarityMat,Interactions_Matrix);
-        Perlman_Dataset((i-1)*nTargets+j,17)=calcualte_feature(i,j,DrugSim_pSideEffectDrugsCommonSimilarityMat,TargetSim_seqTargetsCommonSimilarityMat,Interactions_Matrix);
+        Perlman_Dataset((i-1)*nTargets+j,15)=calculate_feature(i,j,DrugSim_pSideEffectDrugsCommonSimilarityMat,TargetSim_GOTargetsCommonSimilarityMat,Interactions_Matrix);
+        Perlman_Dataset((i-1)*nTargets+j,16)=calculate_feature(i,j,DrugSim_pSideEffectDrugsCommonSimilarityMat,TargetSim_distTargetsCommonSimilarityMat,Interactions_Matrix);
+        Perlman_Dataset((i-1)*nTargets+j,17)=calculate_feature(i,j,DrugSim_pSideEffectDrugsCommonSimilarityMat,TargetSim_seqTargetsCommonSimilarityMat,Interactions_Matrix);
         
         Perlman_Dataset((i-1)*nTargets+j,18)=Interactions_Matrix_Full(i,j);
         
